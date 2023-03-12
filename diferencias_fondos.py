c3535e40bbe3b2094a4e50c6252ae60e30fff107
RodrigoFernandez <rdr.rkh+github2@gmail.com>
2023-03-12 19:47:02 -0300

    Modificación para obtener los datos para la diferencia entre fondos directamente desde el archivo de configuración toml

--- diferencias_fondos.py
+++ diferencias_fondos.py
@@ -2,21 +2,16 @@
 diferencias_fondos.py
 """
 
-import argparse
+from pathlib import Path
+# https://realpython.com/python-toml/
+import tomli
 
 from diferenciador.diferenciadores import Diferenciador
 
 
-def get_parser():
-    parser = argparse.ArgumentParser(description="Diferencias fondos csv")
-    parser.add_argument('fondos', help='archivo csv con las cotizaciones de los fondos')
-    parser.add_argument('--salida', help='Nombre csv con la salida',
-                        required=False, default='diferencias_fondos_comunes.csv')
-    return parser
-
-
 if __name__ == '__main__':
-    args = get_parser().parse_args()
+    configuracion = tomli.loads(Path(".fondos_comunes.toml").read_text(encoding='utf-8'))
 
-    diferenciador = Diferenciador(args.fondos, args.salida)
+    diferenciador = Diferenciador(configuracion["diferenciador"]["fondos"],
+                                  configuracion["diferenciador"]["salida"])
     diferenciador.ejecutar()
